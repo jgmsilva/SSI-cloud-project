2dee146800107f2a0f3afe5b6381f480f9509b6c
SALIPE <felipebuenosouza@uol.com.br>
2024-11-19 21:18:14 +0000

    cloud init vm base

--- main.tf
+++ main.tf
@@ -1,47 +1,48 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "bpg/proxmox"
+      source  = "bpg/proxmox"
       version = "0.66.3"
     }
   }
 }
 
 provider "proxmox" {
- endpoint  = "https://192.168.122.29:8006/api2/json"
- username  = "root@pam"
- password  = "proxmox"
- insecure  = true
- ssh {
+  endpoint = "https://192.168.122.243:8006/api2/json"
+  username = "root@pam"
+  password = "proxmox"
+  insecure = true
+  ssh {
     agent = true
- }
+  }
 }
 resource "tls_private_key" "rsa-4096-audit-key" {
   algorithm = "RSA"
-  rsa_bits = 4096
+  rsa_bits  = 4096
 }
 resource "proxmox_virtual_environment_vm" "audit_vm" {
-  name = "audit"
- node_name  = var.proxmox_nodename
- stop_on_destroy = true
+  name            = "audit"
+  node_name       = var.proxmox_nodename
+  stop_on_destroy = true
   initialization {
-  user_account {
-    keys = [chomp(tls_private_key.rsa-4096-audit-key.private_key_openssh)]
+
+    user_account {
+      # do not use this in production, configure your own ssh key instead!
+      username = "user"
+      password = "password"
+    }
   }
-  }
- clone {
-    vm_id   = 100
- }
- disk {
+
+  disk {
     interface    = "virtio0"
     size         = 20
     datastore_id = "local-lvm"
-    file_format = "raw"
- }
+    file_format  = "raw"
+  }
 
- cpu {
-    cores        = 2
-    type         = "x86-64-v2-AES"  # recommended for modern CPUs
+  cpu {
+    cores = 2
+    type  = "x86-64-v2-AES" # recommended for modern CPUs
   }
 
   memory {
@@ -49,29 +50,36 @@ resource "proxmox_virtual_environment_vm" "audit_vm" {
     floating  = 2048 # set equal to dedicated to enable ballooning
   }
 }
+
+
 resource "proxmox_virtual_environment_vm" "client_vm" {
-  for_each   = try(var.clientvm_object, {})
- name       = each.value.name
- node_name  = var.proxmox_nodename
+  for_each  = try(var.clientvm_object, {})
+  name      = each.value.name
+  node_name = var.proxmox_nodename
+
   initialization {
-  user_account {
-  keys = [chomp(tls_private_key.rsa-4096-audit-key.public_key_openssh)]
+    user_account {
+      # do not use this in production, configure your own ssh key instead!
+      username = "user"
+      password = "password"
+    }
   }
-  }
- stop_on_destroy = true
- clone {
-    vm_id   = 100
- }
- disk {
+  stop_on_destroy = true
+  # clone {
+  #   vm_id = 100
+  # }
+  disk {
     interface    = "virtio0"
     size         = 20
     datastore_id = "local-lvm"
-    file_format = "raw"
- }
+    iothread     = true
+    discard      = "on"
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+  }
 
- cpu {
-    cores        = 2
-    type         = "x86-64-v2-AES"  # recommended for modern CPUs
+  cpu {
+    cores = 2
+    type  = "x86-64-v2-AES" # recommended for modern CPUs
   }
 
   memory {
@@ -81,3 +89,10 @@ resource "proxmox_virtual_environment_vm" "client_vm" {
 
 }
 
+
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "pve"
+  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,14 +1,14 @@
-proxmox_nodename = "myproxmox"
+proxmox_nodename = "pve"
 
 clientvm_object = {
   "vm1" = {
-    name = "vm1"
-    role = "web-server"
+    name   = "vm1"
+    role   = "web-server"
     memory = 512
   }
-  "vm2" = {
-    name = "vm2"
-    role = "database"
-    memory = 512
-  }
+  # "vm2" = {
+  #   name = "vm2"
+  #   role = "database"
+  #   memory = 512
+  # }
 }
