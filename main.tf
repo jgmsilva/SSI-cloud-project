a3a3418c79fde33319e909c80de299ba9d55d434
SALIPE <felipebuenosouza@uol.com.br>
2024-12-13 02:11:09 +0000

    enforcing

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
 
 resource "tls_private_key" "ubuntu_vm_key" {
   algorithm = "RSA"
-  rsa_bits  = 2048
+  rsa_bits  = 4096
 }
 
 locals {
